2e68a6554f03733d3a9e646acfa0f8eebf39cf5a
Max Karou <maxkarou@protonmail.com>
2025-07-06 00:45:35 +0200

    feat: added `firefox` module

---
+++ modules/home/firefox.nix
@@ -0,0 +1,70 @@
+{
+  lib,
+  osConfig,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (osConfig) wayland;
+in
+mkIf wayland.enable {
+  # set environment variables (user)
+  home.sessionVariables = {
+    # set default browser
+    BROWSER = "firefox";
+
+    # enable GPU rendering (firefox)
+    MOZ_WEBRENDER = "1";
+
+    # enable wayland support (firefox)
+    MOZ_ENABLE_WAYLAND = "1";
+  };
+
+  # enable firefox, see: https://www.mozilla.org/en-US/firefox/new/
+  programs.firefox = {
+    enable = true;
+
+    # set policies
+    policies = {
+      # disable updates
+      AppAutoUpdate = false;
+      BackgroundAppUpdate = false;
+
+      # allow features
+      DisableFirefoxAccounts = false;
+      DisableBuiltinPDFViewer = false;
+
+      # disable features
+      DisablePocket = true;
+      DisableTelemetry = true;
+      OfferToSaveLogins = false;
+      DisableFirefoxStudies = true;
+      DontCheckDefaultBrowser = true;
+
+      # enable tracking protection
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+
+      # install extensions
+      ExtensionUpdate = false;
+      ExtensionSettings =
+        let
+          mkExtension = abbrvId: uuid: {
+            name = uuid;
+            value = {
+              install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${abbrvId}/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+          };
+        in
+        builtins.listToAttrs [
+          (mkExtension "ublock-origin" "uBlock0@raymondhill.net")
+        ];
+    };
+  };
+}
