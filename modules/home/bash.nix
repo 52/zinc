d610f1c04853ca05094d711e5cb9914c016a24b9
Max Karou <maxkarou@protonmail.com>
2025-07-06 00:31:17 +0200

    feat: modified 'home/bash.nix' definition

--- modules/home/bash.nix
+++ modules/home/bash.nix
@@ -1,25 +1,108 @@
 {
-  lib,
   config,
   ...
 }:
-let
-  inherit (lib) mkOption mkIf types;
-  cfg = config.bash;
-in
 {
-  options.bash = {
-    enable = mkOption {
-      type = types.bool;
-      description = "Whether to enable the 'bash' module";
-      default = true;
-    };
-  };
+  programs.bash = {
+    enable = true;
 
-  config = mkIf cfg.enable {
-    programs.bash = {
-      enable = true;
-      historyFile = "${config.xdg.dataHome}/bash/bash_history";
+    # limit the number of lines (memory)
+    historySize = 500000;
+
+    # limit the number of lines (flushed)
+    historyFileSize = 100000;
+
+    # remove the `.bash_history` file from $HOME
+    historyFile = "${config.xdg.dataHome}/bash/bash_history";
+
+    # set features (history)
+    historyControl = [
+      # ignore duplicate entries
+      "ignoredups"
+
+      # remove duplicate entries
+      "erasedups"
+    ];
+
+    # set features (shell)
+    shellOptions = [
+      # enable recursive globbing
+      "globstar 2> /dev/null"
+
+      # enable case-insensitive globbing
+      "nocaseglob;"
+
+      # append to history file
+      "histappend"
+
+      # update window frequently
+      "checkwinsize"
+
+      # save multi-line commands as one entry
+      "cmdhist"
+
+      # prepend 'cd' to directory names
+      "autocd 2> /dev/null"
+
+      # correct spelling errors during 'cd'
+      "cdspell 2> /dev/null"
+
+      # correct spelling errors during tab-completion
+      "dirspell 2> /dev/null"
+    ];
+
+    # set aliases
+    shellAliases = {
+      ".." = "cd ..";
+
+      # disk
+      df = "df -h";
+      du = "du -h -c";
+
+      # list
+      ls = "ls -lF -G --group-directories-first --color=auto";
+      la = "ls -lAF -G --group-directories-first --color=auto";
+
+      # git
+      g = "git";
+      ga = "git add";
+      gp = "git push";
+      gd = "git diff";
+      gc = "git commit";
+      gs = "git status";
+      gb = "git branch";
+      gl = "git log --decorate --oneline --graph";
+
+      # nix
+      n = "nix";
+      nd = "nix develop";
+
+      # tmux
+      t = "tmux";
+      tl = "tmux ls";
+      td = "tmux detach";
+      ta = "tmux attach -t";
     };
+
+    # enable 'bash-sensible', see: https://github.com/mrzool/bash-sensible/
+    interactiveShellInit = ''
+      # enable case-insensitve completion
+      bind "set completion-ignore-case on"
+
+      # treat hyphens and underscores equally
+      bind "set completion-map-case on"
+
+      # display all matches immediatly
+      bind "set show-all-if-ambiguous on"
+
+      # append slash to symlinked directories
+      bind "set mark-symlinked-directories on"
+
+      # enable incremental history search
+      bind '"\e[A": history-search-backward'
+      bind '"\e[B": history-search-forward'
+      bind '"\e[C": forward-char'
+      bind '"\e[D": backward-char'
+    '';
   };
 }
