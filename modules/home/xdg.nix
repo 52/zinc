8cfa6b50c1c0e356108f6be0233473665d607cad
Max Karou <maxkarou@protonmail.com>
2025-07-06 00:45:57 +0200

    feat: added `xdg` module

---
+++ modules/home/xdg.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  ...
+}:
+{
+  xdg = {
+    enable = true;
+
+    # configure the 'user_dirs.dirs' file
+    userDirs = {
+      enable = true;
+
+      # automatically create directories
+      createDirectories = true;
+
+      documents = "${config.home.homeDirectory}/Documents";
+      download = "${config.home.homeDirectory}/Downloads";
+      desktop = "${config.home.homeDirectory}/Desktop";
+
+      pictures = "${config.home.homeDirectory}/Media/Images";
+      videos = "${config.home.homeDirectory}/Media/Video";
+      music = "${config.home.homeDirectory}/Media/Audio";
+
+      # disable $XDG_PUBLICSHARE_DIR and $XDG_TEMPLATES_DIR
+      extraConfig = {
+        XDG_PUBLICSHARE_DIR = "/var/empty";
+        XDG_TEMPLATES_DIR = "/var/empty";
+      };
+    };
+  };
+
+  # force programs to use XDG directories (when possible)
+  home.preferXdgDirectories = true;
+}
