08fac97783b7502c26a3321e1e65f99408c0963b
Max Karou <maxkarou@protonmail.com>
2025-07-07 15:18:51 +0200

    feat: added `git` module

---
+++ modules/home/git.nix
@@ -0,0 +1,79 @@
+{ ... }:
+{
+  # enable git, https://git-scm.com/
+  programs.git = {
+    enable = true;
+
+    extraConfig = {
+      core = {
+        # enable file-system monitor
+        fsmonitor = true;
+
+        # enable untracked file caching
+        untrackedCache = true;
+      };
+
+      push = {
+        # push only current branch
+        default = "simple";
+
+        # automatically create remote branch
+        autoSetupRemote = true;
+
+        # push tags with commits
+        followTags = true;
+      };
+
+      rebase = {
+        # automatically squash commits
+        autoSquash = true;
+
+        # automatically stash before rebase
+        autoStash = true;
+
+        # automatically update refs
+        updateRefs = true;
+      };
+
+      diff = {
+        # use histogram diff algorithm
+        algorithm = "histogram";
+
+        # display moved lines in color
+        colorMoved = "plain";
+
+        # use descriptive prefixes
+        mnemonicPrefix = true;
+
+        # detect renamed files
+        renames = true;
+      };
+
+      commit = {
+        # cleanup at scissors marker
+        cleanup = "scissors";
+
+        # display entire diff in editor
+        verbose = true;
+      };
+
+      # set default branch to 'master'
+      init.defaultBranch = "master";
+
+      # sort branches by most recent commit
+      branch.sort = "-comitterdate";
+
+      # sort tags by most recent commit
+      tag.sort = "-comitterdate";
+
+      # enable columized output
+      column.ui = "auto";
+
+      # enable colored output
+      color.ui = "auto";
+
+      # enable auto-correct
+      help.autocorrect = 1;
+    };
+  };
+}
