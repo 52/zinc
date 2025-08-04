4b29ef97f86c5f7a6ae3472ef3cb32b9d93135a4
Max Karou <maxkarou@protonmail.com>
2025-08-04 20:15:39 +0200

    feat: updated `bin` files

---
+++ modules/home/bin.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  # List of scripts inside the 'bin' directory.
+  scripts = lib.attrNames (
+    lib.filterAttrs (_: type: type == "regular") (builtins.readDir (lib.relativePath "bin"))
+  );
+in
+{
+  # Install all scripts, see: 'bin' folder.
+  home.packages = builtins.map (
+    name: pkgs.writeShellScriptBin name (lib.readFileRelative "bin/${name}")
+  ) scripts;
+}
