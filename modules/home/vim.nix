81332cb4b53275b7e1425ac214801d0c9f8a8bce
Max Karou <maxkarou@protonmail.com>
2025-08-04 20:39:57 +0200

    docs: remove period from comment

--- modules/home/vim.nix
+++ modules/home/vim.nix
@@ -6,7 +6,7 @@
   # Set the default editor.
   env.EDITOR = "vim";
 
-  # Enable vim, see: https://www.vim.org/.
+  # Enable vim, see: https://www.vim.org/
   home.packages = builtins.attrValues {
     inherit (pkgs)
       vix
